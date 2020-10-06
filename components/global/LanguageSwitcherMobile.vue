12a2486f30570a71f049e63f986922dd589a1124
Niz <62140571+nicosapp@users.noreply.github.com>
2020-10-06 11:45:29 +0200

    fix mobile switcher

--- components/global/LanguageSwitcherMobile.vue
+++ components/global/LanguageSwitcherMobile.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="flex items-center justify-between">
     <a
-      v-for="locale in ['fr', 'en']"
-      :key="locale"
       href="#"
       class="text-gray-500"
-      :class="{ 'text-gray-900': $i18n.locale === locale }"
+      :class="{ 'text-gray-900': $i18n.locale === 'fr' }"
       @click.prevent="$i18n.setLocale(locale)"
     >
-      {{ locale.toUpperCase() }}
+      FR
+    </a>
+    <a
+      href="#"
+      class="text-gray-500"
+      :class="{ 'text-gray-900': $i18n.locale === 'en' }"
+      @click.prevent="$i18n.setLocale(locale)"
+    >
+      EN
     </a>
   </div>
 </template>
